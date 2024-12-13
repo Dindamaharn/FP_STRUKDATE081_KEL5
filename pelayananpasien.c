508dd577d75c7c3c865150293192036fd36b4a86
Fajrul Falaq Z. Malangke <fajrulfalaq614@gmail.com>
2024-12-13 16:23:45 +0700

    main program
    :p

--- pelayananpasien.c
+++ pelayananpasien.c
@@ -71,3 +71,102 @@ void moveToActiveQueue(Queue *queue, DoubleLinkedList *activeQueue)
 
     printf("Pasien %s (ID: %d) dipindahkan ke antrian aktif.\n", nextPatient->name, nextPatient->id);
 }
+
+//main programm
+int main() {
+    Queue waitingQueue ={NULL, NULL};
+    DoubleLinkedList activeQueue = {NULL, NULL};
+    SingleLinkedList history = {NULL};
+    int choice, id, age, priority;
+    char name[50], gender[10], status[20];
+
+    while (1)
+    {
+        printf("\n ------ Menu ------\n");
+        printf("1. Tambahkan pasien ke registrasi\n");
+        printf("2. Pindahkan pasien ke antrian aktif\n");
+        printf("3. Cetak antrian registrasi\n");
+        printf("4. Cetak antrian pasien aktif\n");
+        printf("5. Catat histori pasien setelah pemeriksaan\n");
+        printf("6. Tampilkan riwayat pasien hari ini\n");
+        printf("7. Cari pasien berdasarkan ID\n");
+        printf("8. Keluar program\n");
+        printf("9. Hapus semua data\n");
+        printf("Pilih opsi: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("Masukkan ID Pasien: ");
+            scanf("%d, &id");
+            printf("Masukkan nama pasien: ");
+            getchar();
+            fgets(name, sizeof(name), stdin);
+            name[strcspn(name, "\n")] = '\0';
+            printf("Masukkan usia pasien: ");
+            scanf("%d", &age);
+            printf("Masukkan jenis kelamin pasien: ");
+            scanf("%s", gender);
+            printf("Masukkan kepentingan pasien (IGD, Kontrol, Konsultasi): ");
+            scanf("%s", status);
+
+            if (strcmp(status, "IGD")==0)
+            {
+                priority = 1;
+            }
+            else if (strcmp(status, "Kontrol")==0)
+            {
+                priority = 2;
+            }
+            else
+            {
+                priority = 3;
+            }
+            
+            enqueue(&waitingQueue, createPatient(id, name, age, gender, status, priority));
+            printf("Pasien berhasil ditambahkan ke antrian registrasi.\n");
+            break;
+
+        case 2:
+            moveToActiveQueue(&waitingQueue, &activeQueue);
+            break;
+
+        case 3:
+            printQueue(&waitingQueue);
+            break;
+
+        case 4:
+            printActiveQueue(&activeQueue);
+            break;
+
+        case 5:
+            moveToHistory(&activeQueue, &history);
+            break;
+
+        case 6:
+            printHistory(&history);
+            break;
+
+        case 7:
+            printf("Masukkan ID pasien yang ingin dicari: ");
+            scanf("%d", &id);
+            searchPatienById(&waitingQueue, &activeQueue, &history, id);
+            break;
+
+        case 8:
+            exit(0);
+            break;
+
+        case 9:
+            clearQueue(&waitingQueue);
+            clearActiveQueue(&activeQueue);
+            clearHistory(&history);
+            break;
+        default:
+            printf("Opsi tidak valid!!\n");
+        }
+    }
+    
+    return 0;
+}
